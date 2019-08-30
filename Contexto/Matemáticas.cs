c61919cbea53dc2d704fecb87876b8ba171d1248
Carlos Encinas <carlos-encinas@hotmail.com>
2019-08-30 08:43:25 -0700

    Agregar archivos de proyecto.

---
+++ Contexto/Matemáticas.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Contexto
+{
+    class Matemáticas
+    {
+        public const double PI = 3.14159;
+        public static double sumar(double sumando1, double sumando2)
+        {
+            return sumando1 + sumando2;
+        }
+
+        public static double areaCirculo(double radio)
+        {
+            return (radio * radio) * PI;
+        }
+    }
+}
---
+++ Contexto/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Contexto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            //Matemáticas matemáticas = new Matemáticas();
+            double sumando1 = 10;
+            double sumando2 = 43;
+            double resultado = Matemáticas.sumar(sumando1 , sumando2);
+
+            Console.WriteLine(sumando1.ToString() + " + " + sumando2.ToString() + " = " + resultado.ToString());
+            Console.WriteLine("PI: " + Matemáticas.PI);
+            //Matemáticas.PI = 95;
+            double radio = 3.67;
+            Console.WriteLine("Radio: " + radio.ToString() + ", Area: " + Matemáticas.areaCirculo(radio));
+            
+            Console.ReadKey();
+
+
+        }
+    }
+}
